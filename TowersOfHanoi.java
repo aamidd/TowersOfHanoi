38e3e609a5417a314f9760050bb4e07b9a036335
Amid Sanai <sanaiamid@gmail.com>
2024-01-14 20:34:47 +0330

    update code

--- TowersOfHanoi.java
+++ TowersOfHanoi.java
@@ -1,23 +1,31 @@
 import java.math.BigInteger;
+import java.util.Scanner;
 public class TowersOfHanoi{
 	public static void main(String[] args) {
-		int disks = Integer.parseInt(args[0]);
-		BigInteger numberOfMoves = BigInteger.TWO.pow(disks).subtract(BigInteger.ONE);
-		int sourcePeg = 1;
-		int tmpPeg = 2;
-		int destinationPeg = 3;
-		solve(disks, sourcePeg, tmpPeg, destinationPeg);
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("> Number of disks: ");
+		int numberOfDisks = input.nextInt();
+
+		System.out.println();
+
+		BigInteger numberOfMoves = BigInteger.TWO.pow(numberOfDisks).subtract(BigInteger.ONE);
+
+		solve(numberOfDisks);
 		System.out.printf("number of moves: %d%n", numberOfMoves);
 	}
 
-	public static void solve(int disks, int srcPeg, int tmpPeg, int dstPeg) {
-		if (disks == 1) {
+	public static void solve(int numberOfDisks, int srcPeg, int tmpPeg, int dstPeg) {
+		if (numberOfDisks == 1) {
 			System.out.printf("%d --> %d%n", srcPeg, dstPeg);
 			return;
 		}
 
-		solve(disks - 1, srcPeg, dstPeg, tmpPeg);
+		solve(numberOfDisks - 1, srcPeg, dstPeg, tmpPeg);
 		System.out.printf("%d --> %d%n", srcPeg, dstPeg);
-		solve(disks - 1, tmpPeg, srcPeg, dstPeg);
+		solve(numberOfDisks - 1, tmpPeg, srcPeg, dstPeg);
+	}
+	private static void solve(int numberOfDisks) {
+		solve(numberOfDisks, 1, 2, 3);
 	}
 }
